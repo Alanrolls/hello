a35c6a8dc192dc1d614f690f08a98a62d9a7cfaa
Alan Estrada-Morales <alanestrafa454@gmail.com>
2025-08-28 09:39:31 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Alan Estrada-Morales
+ *         EMAIl aestrada-morales2@unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+
+ #include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Alan Estrada-Morales\n");
+
+return 0;
+}
