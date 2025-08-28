da34331903510fd6a81a2c34411deaed4fea242a
Alan Estrada-Morales <alanestrafa454@gmail.com>
2025-08-28 10:14:39 -0500

    added my major to the c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
  * Author: Alan Estrada-Morales
- *         EMAIl aestrada-morales2@unl.edu
- * Date: 2025/08/26
+ *         EMAIl aestrada-morales2@huskers.unl.edu
+ * Date: 2025/08/28
  *
  * A simple hello world program in C
  *
@@ -12,7 +12,8 @@
 
 int main(int argc, char **argv) {
 
-printf("Alan Estrada-Morales\n");
+printf("Alan Estrada-Morales!!\n");
+printf("My major is Computer Science!!!\n");
 
 return 0;
 }
